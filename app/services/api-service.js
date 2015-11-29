e3eb4c911c465bbba88c4dd574a59b9f7a1ddc44
robabby <hello@robabby.co>
2015-11-29 15:03:21 -0600

    Formatting

--- app/services/api-service.js
+++ app/services/api-service.js
@@ -5,12 +5,12 @@ let apiUrl = "https://hacker-news.firebaseio.com/v0/";
 export default Ember.Service.extend({
   getTopStories() {
     return Ember.$.getJSON(apiUrl + "topstories.json").then(function(data){
-        return data;
-      });
+      return data;
+    });
   },
   getNewStories() {
     return Ember.$.getJSON(apiUrl + "newstories.json").then(function(data){
-        return data;
-      });
+      return data;
+    });
   }
 });
