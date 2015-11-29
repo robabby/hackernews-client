001d5b0924728e441553d766c9d0fd14929bbea4
robabby <hello@robabby.co>
2015-11-28 21:30:58 -0600

    Implement Register route

--- app/register/route.js
+++ app/register/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew: function() {
+      var user = this.store.createRecord('user', {
+        name : this.controller.get('name')
+      });
+      user.save().then(() => {
+        console.log('Save successful!');
+        this.controller.set('message',
+          'A new user with the name "' + this.controller.get('name') + '" was added!'
+        );
+      }, function() {
+        console.log('Save failed');
+      });
+    }
+  }
 });
